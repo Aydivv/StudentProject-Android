d42afa29437b9791e8518a3f7ac5473dbc278fc6
Aydiv <55044178+Aydivv@users.noreply.github.com>
2021-11-19 01:48:28 +0000

    Home page added

--- app/src/main/java/com/example/farmbnb/MainActivity.java
+++ app/src/main/java/com/example/farmbnb/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.farmbnb;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goHomepage(View view){
+        Intent home = new Intent(this,home.class);
+        startActivity(home);
     }
 }
