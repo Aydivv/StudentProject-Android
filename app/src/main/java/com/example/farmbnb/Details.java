58de70422cc9b0584c1b7674ee2bcab0b43066e9
Aydiv <55044178+Aydivv@users.noreply.github.com>
2021-11-20 01:32:47 +0000

    Details done with dates and colors

--- app/src/main/java/com/example/farmbnb/Details.java
+++ app/src/main/java/com/example/farmbnb/Details.java
@@ -1,9 +1,16 @@
 package com.example.farmbnb;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.icu.util.Calendar;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 public class Details extends AppCompatActivity {
 
@@ -13,8 +20,46 @@ public class Details extends AppCompatActivity {
         setContentView(R.layout.activity_details);
     }
 
-    public void goDates(View view){
-        Intent goDates = new Intent(this,Dates.class);
-        startActivity(goDates);
+    public void goPayment(View view){
+        Intent goPayment = new Intent(this,Payment.class);
+        startActivity(goPayment);
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void datePickerArrival(View view){
+        EditText date;
+        date = (EditText) findViewById(R.id.arrivalDate);
+        final Calendar c = Calendar.getInstance();
+        int tYear = c.get(Calendar.YEAR);
+        int tMonth = c.get(Calendar.MONTH);
+        int tDay = c.get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog dpd = new DatePickerDialog(Details.this,R.style.DatePickerTheme,
+                new DatePickerDialog.OnDateSetListener(){
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth){
+                        date.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                    }
+                },tYear,tMonth,tDay);
+        dpd.show();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void datePickerDeparture(View view){
+        EditText date;
+        date = (EditText) findViewById(R.id.departureDate);
+        final Calendar c = Calendar.getInstance();
+        int tYear = c.get(Calendar.YEAR);
+        int tMonth = c.get(Calendar.MONTH);
+        int tDay = c.get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog dpd = new DatePickerDialog(Details.this,R.style.DatePickerTheme,
+                new DatePickerDialog.OnDateSetListener(){
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth){
+                        date.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                    }
+                },tYear,tMonth,tDay);
+        dpd.show();
+    }
+
+
 }
--- app/src/main/java/com/example/farmbnb/Payment.java
+++ app/src/main/java/com/example/farmbnb/Payment.java
@@ -10,4 +10,6 @@ public class Payment extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
     }
+
+
 }
