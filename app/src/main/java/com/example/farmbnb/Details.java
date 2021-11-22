70a83807b6add0736d5885de905b641ed5395a2e
Aydiv <55044178+Aydivv@users.noreply.github.com>
2021-11-22 08:51:09 +0000

    cleaning up code

--- app/src/main/java/com/example/farmbnb/Details.java
+++ app/src/main/java/com/example/farmbnb/Details.java
@@ -1,18 +1,12 @@
 package com.example.farmbnb;
 
-import static com.example.farmbnb.bookings.fromDate;
+
 
 import android.app.DatePickerDialog;
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.content.Intent;
-import android.graphics.Color;
 import android.icu.util.Calendar;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,9 +14,9 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
+
+
 public class Details extends AppCompatActivity {
-    NotificationManagerCompat nMC;
-    Notification aReminder,dReminder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +24,8 @@ public class Details extends AppCompatActivity {
         setContentView(R.layout.activity_details);
     }
 
-    public void goPayment(View view){
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void goPayment(View view) {
         EditText noOfPeople = (EditText) findViewById(R.id.detailNOP);
         EditText noOfChildren = (EditText) findViewById(R.id.detailNOC);
         EditText arrival = (EditText) findViewById(R.id.arrivalDate);
@@ -47,7 +42,8 @@ public class Details extends AppCompatActivity {
             arrival.setError("Cannot be empty");
         } else if (departure.length()==0){
             departure.setError("Cannot be empty");
-        } else {
+        }
+        else{
             Toast.makeText(Details.this,"Reserved",Toast.LENGTH_SHORT).show();
             Intent goPayment = new Intent(this,Payment.class);
             goPayment.putExtra("aDate",aDate);
@@ -55,6 +51,7 @@ public class Details extends AppCompatActivity {
             startActivity(goPayment);
         }
     }
+
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     public void datePickerArrival(View view){
