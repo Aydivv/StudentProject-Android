5f8fc5c7f531f9aec4b164094a9932c1c05ee6db
Aydiv <55044178+Aydivv@users.noreply.github.com>
2021-12-26 06:50:33 +0000

    Add Project activity created.
    Initial XML for Add project added.

---
+++ CW2/app/src/main/java/com/example/projudent/addProject.java
@@ -0,0 +1,14 @@
+package com.example.projudent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addProject extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_project);
+    }
+}
