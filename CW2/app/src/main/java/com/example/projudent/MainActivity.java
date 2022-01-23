d4b5fef4e92cd8dae7172c982ceed5b075cfb9b0
Aydiv <55044178+Aydivv@users.noreply.github.com>
2022-01-23 01:42:20 +0000

    Log In system added

--- CW2/app/src/main/java/com/example/projudent/MainActivity.java
+++ CW2/app/src/main/java/com/example/projudent/MainActivity.java
@@ -3,21 +3,37 @@ package com.example.projudent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.media.effect.Effect;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.opencsv.CSVReader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private EditText loginID;
     private EditText loginPassword;
     private TextView btnLogin;
     private User user;
+    private ArrayList<Integer> IDs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        IDs = new ArrayList<Integer>();
     }
 
     public void toRegister(View view) {
@@ -27,11 +43,89 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void toWelcome(View view) {
+        boolean login = false;
         loginID = findViewById(R.id.etStudentID);
         loginPassword = findViewById(R.id.etPassword);
-        user = new User(Integer.parseInt(loginID.getText().toString()),"newname","lastname",loginPassword.getText().toString());
-        Intent intent = new Intent(this, welcome.class);
-        intent.putExtra("User",user);
-        startActivity(intent);
+        if(loginID.length()==0){
+            loginID.setError("Enter StudentID");
+        }
+        else if(loginPassword.length()==0){
+            loginPassword.setError("Enter Password");
+        }
+        else {
+            int ID = Integer.parseInt(loginID.getText().toString());
+            String pword = loginPassword.getText().toString();
+            login = checkLogin(ID, pword);
+
+            if (login) {
+                Toast.makeText(this, "correct", Toast.LENGTH_SHORT).show();
+                ArrayList<String> names = getNames(ID);
+                user = new User(ID, names.get(0),names.get(1), pword);
+                Intent intent = new Intent(this, welcome.class);
+                intent.putExtra("User", user);
+                startActivity(intent);
+            } else {
+                boolean exists = false;
+                for (int x : IDs) {
+                    if (x == ID)
+                        exists = true;
+                }
+                if (exists) {
+                    Toast.makeText(this, "Incorrect Password.", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "StudentID does not exist. Please Register.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
+
+    public boolean checkLogin(int ID, String pword){
+        boolean value = false;
+        try {
+            FileInputStream is = openFileInput("users.txt");
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(is, StandardCharsets.UTF_8)
+            );
+            String user;
+            while ((user = reader.readLine()) != null) {
+                String[] tokens = user.split(",");
+                IDs.add(Integer.parseInt(tokens[0]));
+                if((Integer.parseInt(tokens[0])==ID) && (tokens[1].equals(pword))){
+                    value = true;
+                }
+
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+            Toast.makeText(this,"error reading file",Toast.LENGTH_SHORT).show();
+        }
+
+        return value;
+    }
+
+    public ArrayList<String> getNames(int ID){
+        ArrayList<String> names = new ArrayList<>();
+        try {
+            FileInputStream is = openFileInput("users.txt");
+            BufferedReader reader = new BufferedReader(
+                new InputStreamReader(is, StandardCharsets.UTF_8)
+        );
+        String user;
+
+            while ((user = reader.readLine()) != null) {
+                String[] tokens = user.split(",");
+                if(Integer.parseInt(tokens[0])==ID){
+                    names.add(tokens[2]);
+                    names.add(tokens[3]);
+                    return names;
+                }
+
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+            Toast.makeText(this,"error reading file",Toast.LENGTH_SHORT).show();
+        }
+        return names;
+    }
+
 }
--- CW2/app/src/main/java/com/example/projudent/createAccount.java
+++ CW2/app/src/main/java/com/example/projudent/createAccount.java
@@ -2,12 +2,25 @@ package com.example.projudent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 
 public class createAccount extends AppCompatActivity {
-
+    private EditText etID;
+    private EditText etpassword;
+    private EditText etrPassword;
+    private EditText etfname;
+    private EditText etlname;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +40,49 @@ public class createAccount extends AppCompatActivity {
     }
 
     public void toWelcome(View view){
-        Intent intent = new Intent(this,welcome.class);
-        startActivity(intent);
+        etID = findViewById(R.id.etStudentIDca);
+        etpassword = findViewById(R.id.etPasswordca);
+        etrPassword = findViewById(R.id.etRepeatPasswordca);
+        etfname = findViewById(R.id.etFirstName);
+        etlname = findViewById(R.id.etLastName);
+
+        if (etID.length() == 0) {
+            etID.setError("Enter Student ID");
+        } else if (etpassword.length() == 0) {
+            etpassword.setError("Enter Password");
+        } else if (etrPassword.length() == 0) {
+            etrPassword.setError("Repeat Password");
+        } else if (!etpassword.getText().toString().equals(etrPassword.getText().toString())) {
+            etrPassword.setError("Password does not match");
+        } else if (etfname.length() == 0) {
+            etfname.setError("Enter First Name.");
+        } else if (etlname.length() == 0) {
+            etlname.setError("Enter Second Name");
+        } else {
+            int ID = Integer.parseInt(etID.getText().toString().trim());
+            String password = etpassword.getText().toString().trim();
+            String fname = etfname.getText().toString().trim();
+            String lname = etlname.getText().toString().trim();
+            createUser(ID,password,fname,lname);
+            Toast.makeText(createAccount.this,"Account Created.",Toast.LENGTH_SHORT).show();
+
+            User user = new User(ID, fname, lname, password);
+            Intent intent = new Intent(this, welcome.class);
+            intent.putExtra("User", user);
+            startActivity(intent);
+        }
+    }
+
+    public void createUser(int ID,String pword,String fname,String lname){
+        try {
+
+            FileOutputStream fop = openFileOutput("users.txt",Context.MODE_APPEND);
+            fop.write((String.valueOf(ID) + "," + pword + "," + fname + "," + lname + "\n").getBytes(StandardCharsets.UTF_8));
+            fop.close();
+            Toast.makeText(createAccount.this,String.valueOf(ID) + "," + pword + "," + fname + "," + lname + "Saved to users.txt.",Toast.LENGTH_SHORT).show();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
     }
 }
